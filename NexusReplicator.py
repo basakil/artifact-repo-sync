f370948a996a9407b7943dffcba8341d5d38094b
gufran.yesilyurt <gufran.yesilyurt@ozu.edu.tr>
2022-09-11 03:41:30 +0300

    Comments added.

--- NexusReplicator.py
+++ NexusReplicator.py
@@ -1,8 +1,13 @@
-import requests, argparse, datetime, os, json, glob, subprocess, tarfile, shutil
+# Update Date: 11 August 2022
+# Contributors: Gufran Yeşilyurt & Talha Eroğlu
+# This version is not optimized and does not function fully as intended due to the problems while loading images to the docker at importDocker().  
+
+import requests, argparse, datetime, os, json, glob, subprocess, tarfile, shutil, time
 from dateutil.parser import parse
 from collections import defaultdict
 
 class nexusReplicator():
+    # Header to use the REST API of Nexus
     headers = {
         'accept': 'application/json',
     }   
@@ -45,19 +50,24 @@ class nexusReplicator():
         else:
             self.parser.print_help()
     
-    # To export all artifacts with -a argument.
-    def exportArtifactHandler(self):  
+    # Helps to send all artifacts to related function.
+    def exportArtifactHandler(self):
+        # To export all artifacts with -a argument.
         if(self.ALLCHECK):       
             #REST API for listing all repositories.
             responseForAll = requests.get(f'{self.URL}/service/rest/v1/repositorySettings', headers=self.headers, auth=('admin', self.PASSWD)).json()
             for repo in responseForAll:
+                # For the export of raw artifacts
                 if(repo.get("type") != "group" and repo.get("format") != "docker" and not self.DOCKERCHECK):
                     self.REPO = repo.get("name")
                     self.exportArtifact()
+
+                # For the export of docker images
                 elif(repo.get("format") == "docker" and self.DOCKERCHECK):
                     self.REPO = repo.get("name")
                     self.HTTP = f'{self.URL.split(":")[1][2:]}:{str(repo.get("docker").get("httpPort"))}/'
                     self.exportDocker()
+        # To export all artifacts without -a argument.
         else:
             self.exportArtifact()  
     
@@ -156,28 +166,33 @@ class nexusReplicator():
 
 
     def exportDocker(self):
+        # Docker requires login to save images locally
         subprocess.run(["docker", "login", self.HTTP, "--username=admin", f'--password={self.PASSWD}'])
         self.params = {
             'repository': self.REPO
         }
+
         # Rest API for getting component with given repository.
         response = requests.get(f'{self.URL}/service/rest/v1/components', params=self.params, headers=self.headers, auth=('admin', self.PASSWD))
         
         if(response.status_code == 404):
             print(f'Repository {self.REPO} not found in {self.URL}/#admin/repository/repositories')
             return
+        
+        # Creating two different folders to find the difference between the old ones and news.
         folderPath = f'{os.getcwd()}/DockerImages'
         os.makedirs(f'{folderPath}/NewImages', exist_ok=True)
         os.makedirs(f'{folderPath}/OldImages', exist_ok=True)
+
         # Parsing JSON response of getComponentAPI according to last modified date and. 
-        # Storing components' name, sha1, lastModified, and downloadUrl in attrList. 
         # Storing parsed response in responses list.
         items = response.json().get("items")
-        attrList = []
-        oldImages = []
+
+        # To make sure there is a response
         if(items != [] and (len(items) == 1 and items[0].get("name")) != '.'):
             for item in items:         
                 for asset in item['assets']:
+                    # If images are newer than the input date, it pulls and saves new images into the NewImages folder.
                     if(self.DATEB < parse(asset['lastModified']).replace(tzinfo=None) and item.get("name") != "."):
                         newImageName = f'{self.HTTP}{item.get("name")}:{item.get("version")}'
                         newImagePath = f'{folderPath}/NewImages/{newImageName.replace("/", "-")}.tar'
@@ -186,61 +201,79 @@ class nexusReplicator():
                         imageTar = tarfile.open(newImagePath)
                         imageTar.extractall(f'{folderPath}/NewImages/{newImageName.replace("/", "-")}')
                         os.remove(newImagePath)
-                        attrList.append((item.get("name").split("/")[-1], item.get("version"), asset.get("lastModified"), newImagePath))  
 
+                    # If images are older than the input date, it pulls and saves old images into the OldImages folder.
                     elif(self.DATEB > parse(asset['lastModified']).replace(tzinfo=None) and item.get("name") != "."):
                         oldImageName = f'{self.HTTP}{item.get("name")}:{item.get("version")}'
                         oldImagePath = f'{folderPath}/OldImages/{oldImageName.replace("/", "-")}.tar'
                         subprocess.run(["docker", "pull", oldImageName])
                         subprocess.run(["docker", "save", "-o", oldImagePath, oldImageName])
+
+                        # It reads manifest jsons and stores them in order to find shared layers later.
                         imageTar = tarfile.open(oldImagePath)
                         with imageTar as tar:
                             for member in tar:
                                 if member.name.endswith('manifest.json'):
                                     self.MANIFESTS.append(json.load(tar.extractfile(member)))
                                     break
-                        oldImages.append((item.get("name").split("/")[-1], item.get("version"), parse(asset.get("lastModified")).replace(tzinfo=None)))
         
+        # Removes old images which are not needed anymore.
         shutil.rmtree(f'{folderPath}/OldImages/')
+
+        # Makes a dictionary by the shared layers from manifest jsons
         for manifestsList in self.MANIFESTS:
             for manifests in manifestsList:
                 for layers in manifests['Layers']:
                     self.LAYERSDICT[layers.split("/")[0]] = manifests['RepoTags']
-            
+        
+        # Writes LAYERSDICT to a file in order to use it when it is needed to import.
         for root,d_names, f_names in os.walk(f'{folderPath}/NewImages'):
             for d in d_names:
                 if d in self.LAYERSDICT:
                     with open(f'{root}/LayersFromImages', 'a') as f:
                         f.write(str(f'{d}:{self.LAYERSDICT[d]}\n'))
                     shutil.rmtree(f'{root}/{d}', ignore_errors=True) 
+            
+            # Tar image folders which are just new layers (Delta Tar) and then removes folders.
             if(root.split("/")[-2] == "NewImages"):
                 tarfile.open(f'{root}.tar.gz', "w:gz").add(root, arcname=os.path.basename(f'{root}/'))
                 shutil.rmtree(root)
-                    
+
+    # It does not function fully as intended. No problem to merge layers and making an image but it does not load to the docker. Since, uploading to the Nexus is also not finished.                
     def importDocker(self):
+        # Delta Tar files are going to be in this folder.
         sourcePath = f'{os.getcwd()}/DockerImages/NewImages'
+        
+        # Images to find shared layers are going to be saved in this folder. 
         savedPath = f'{os.getcwd()}/DockerImages/SavedImages'
         imageNames = {}
 
+        # To make sure this folder exists.
         if not os.path.exists(savedPath):
             os.makedirs(savedPath)
 
-        # To make sure it checks LayersFromImages
+        # To make sure it checks LayersFromImages.
         layersFlag = True
         while(layersFlag):
             for root,d_names, f_names in os.walk(sourcePath):
-                for files in f_names:     
+                for files in f_names:
+                    # Extracts and removes tar files.     
                     if(files.endswith(".tar.gz")):
                         tarfile.open(f'{root}/{files}').extractall(f'{root}/{files[0:-7]}')                    
                         os.remove(f'{root}/{files}')
                     if(files.endswith("LayersFromImages")):
+                        # To exit from the while loop.
                         layersFlag = False
                         with open(f'{root}/{files}') as f:
+                            # To check if the LayersFromImages is from another folder.
                             valtemp = None
                             for line in f:
+                                # Dictionary key value according to the {sha:image}
                                 (key, val) = line.split("['")
                                 imagePath = f'{savedPath}/{val[0:-3].replace("/", "-")}.tar'
                                 dirSource = f'{imagePath[:-4]}/{key[0:-1]}'
+                                
+                                # Copies the folder if key value matches 
                                 if(valtemp == val and key[0:-1]):
                                     shutil.copytree(dirSource, f'{root}/{key[0:-1]}')
                                 else:
@@ -256,31 +289,40 @@ class nexusReplicator():
                         os.remove(f'{root}/{files}')
                                 
         shutil.rmtree(savedPath)
-
+        
+        # From this line to 304 are experimental and does not fully function.
         for root,d_names, f_names in os.walk(sourcePath):
             if(root.split("/")[-2] == "NewImages"):
                 tarfile.open(f'{root}.tar.gz', "w:gz").add(root, arcname=os.path.basename(f'{root}/'))
-                subprocess.run(["docker", "load", "--input", f'{root}.tar.gz'])
-                shutil.rmtree(root)
+                # subprocess.run([f'docker load -i {root}.tar.gz'], shell=True)
+                # subprocess.run(["whoami"], shell=True)
+                # shutil.rmtree(root)
+        
+        # Throws "error: error processing tar file(exit status 1): unexpected eof" except content3 (It is a test image in my computer and may be different in yours)
+        # Exact same command works from the console and even though folders are identical only one of them works and others throw error.
+        subprocess.run([f'docker load -i /home/gufranyes/artifact-repo-sync/DockerImages/NewImages/192.168.100.10:8092-repository-dockertest2-nginx2:content3.tar.gz'], shell=True)
+        subprocess.run([f'docker load -i /home/gufranyes/artifact-repo-sync/DockerImages/NewImages/192.168.100.10:8090-repository-dockertest-nginx:content2.tar.gz'], shell=True)
+        subprocess.run([f'docker load -i /home/gufranyes/artifact-repo-sync/DockerImages/NewImages/192.168.100.10:8091-repository-dockertest1-nginx1:content2.tar.gz'], shell=True)
+        subprocess.run([f'docker load -i /home/gufranyes/artifact-repo-sync/DockerImages/NewImages/192.168.100.10:8092-repository-dockertest2-nginx22:content4.tar.gz'], shell=True)
 
         print("-----------------------------------------")
         print("Finished Successfully")
         print("-----------------------------------------")
 
 
-# Yapılacaklar:
-# -Export Ederken-
-# (DONE) Girilen tarihten sonra ve önce olan imageları ayrı klasörlerde savele
-# (DONE) Tarların içindeki manifest dosyalarını karşılaştırarak delta oluştur.
-#       (DONE) Ortak olan imageları {sha:image} şeklinde tutabilirsin.
-#       (DONE) Old Manifestlerin içindekileri klasörle kıyaslayıp aynı olanları sil 
-# (DONE) Delta klasörleri haricindekileri sil ve deltaları tarla
-# -Import Ederken-
-# (DONE) Deltaları extractle
-# (DONE) Delta tarlarda yazan imageları savele ve gereken layerları delta'nın içine at
-# (DONE) Son savelenen image'ın dosyalarını sil
-# (ERROR)Deltayı tarlayıp dockera loadla
-# Loadlanan image'ı nexus'a pushla
+# TODO List:
+# -Export-
+# (DONE) Save images seperately which are older and newer than the input date.
+# (DONE) Make a delta by comparing manifest jsons inside tar files.
+#       (DONE) Keep shared images as {sha:image}.
+#       (DONE) Compare the folders with old images and remove the shared ones.
+# (DONE) Remove everything except delta folders and tar them.
+# -Import-
+# (DONE) Extract delta tars.
+# (DONE) Save images required for delta folders and copy them to the delta folder.
+# (DONE) Remove the last saved image folder.
+# (ERROR)Compress and tar the new image and load it to the Docker.
+# Push the loaded image to the Nexus.
 
 
 if __name__ == '__main__':  
